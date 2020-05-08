26c788c68c11c2322dc1cdd63a7a643807014ced
Shady Mohamed <shadymo@icloud.com>
2020-05-08 14:36:29 -0400

    added tasks and fixed backgroundColor

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ const task = document.getElementById('task');
 next.addEventListener('click', handleTasks);
 
 function handleTasks(){
+    var { tasks } = taskHolder
+    var num = Math.floor(Math.random()* tasks.length)
+    task.innerText = tasks[num]
     
-    task.innerText = 'handled'
-    console.log(Math.random());
 }
 
 const taskHolder = {
-    personal: [
-        'Drink 2 Shots',
-        'Pick a player for Rock Paper Scissors, loser takes 2 shots'
-    ],
-    pick: [
-
-    ],
-
+    tasks: [
+        'Take a shot',
+        'Take 2 shots',
+        'Pick someone to take 2 shots',
+        'Pick someone to take a shot with you',
+        'Pick someone and you both take as many shots as they want',
+        'Everyone takes a shot!',
+        'Everyone but this person takes a shot',
+        'All guys drink',
+        'All girls drink',
+        'Never have I ever',
+        'WATERFALLLL!'
+    ]
 }
